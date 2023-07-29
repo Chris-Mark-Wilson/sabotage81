404657966f703f0d84c489828ed531829fcd7ea8
bigsegs <cw.plastering.services@gmail.com>
2023-07-29 10:31:55 +0100

    now I too have a machine gun... well, a bomb, but hey.. thoughts for the future..

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ work to date..
 uxb boxes grid co-ordinates held in state in an array - "boxes"
 position of boxes determined randomly on 1st render via a useState function. max 300 boxes to start. Function checks to see if it is trying to place a box over an existing box, if so it increases the maxboxes by 1 to ensure 300 boxes get placed on the screen.
 This may be the cause of a glitch that appears when in certain areas of the screen, there may well be 2 "divs" at that same co-ordinate..
+Its not a 'bug'.. its a feature :) 
 
 collision detection by comparing grid co-ordinates of required move to array, if co-ordinates exist in array, no move is possible as this means the grid square you are attempting to move into contains a uxb box..
 
 keys to move are q-up, a-down, o-left, p-right
 more zxSpectrum than zx81 but I prefer the chuckie egg layout ;)
+
+trying to create a new div on the fly to serve as the 'bomb', isnt working at the minute... next job... fix it...
+solution (dreampt up while trying to sleep) - carry the bomb with you, you always had it anyway, isnt that the point of the game? then just set a setTimeout(countdown,1000) to tick the bomb and when it reaches zero... BOOOOOM :) 
+And the good thing about that, is that you can RUN AWAY because the event loop is looking after the ticking so its NON BLOCKING CODE!!
+same thing to move the guard!!!
+so.. onwards..
+cool, now Ive got  bomb.. I rendered the bomb before "me" so that you see me and not the bomb when Im running around the screen, carrying it with me!
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import { useRef,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef,useEffect,createElement } from 'react'
+
 import './App.css'
 
 
@@ -10,31 +9,35 @@ const getRnd=()=>{
  }
 
 const getGridElement=(x,y,boxes)=>{
-for(let i=0;i<boxes.length;i++){
-  if(boxes[i][0]===x && boxes[i][1]===y) return true
-}
-return false;
+    for(let i=0;i<boxes.length;i++){
+      if(boxes[i][0]===x && boxes[i][1]===y) return true
+    }
+     return false;
 }
 
-function App() {
+const App=()=>{
   const [myX,setMyX]=useState(getRnd())
   const [myY,setMyY]=useState(getRnd())
   const [guardX,setGuardX]=useState(getRnd())
   const [guardY,setGuardY]=useState(getRnd())
-  const inputRef=useRef(null)
+  const [bombX,setBombX]=useState(myX)
+  const[bombY,setBombY]=useState(myY)
+  const[bombSet,setBombSet]=useState(false)
+
+  const inputRef=useRef(null) // used to set focus on player at start
   useEffect(() => {
     inputRef.current.focus();
 }, []);
 
+  // state for array of uxb boxes
   const [boxes,setBoxes]=useState(()=>{
     let boxes=[]
     let max=300
     for(let i=0;i<max;i++){
-    
       let x=Math.floor(Math.random()*31)
       let y=Math.floor(Math.random()*31)
-     
-      for (let o=0;o<boxes.length;o++){
+    //check for duplication/overlay 
+    for (let o=0;o<boxes.length;o++){
         if(boxes[o][0]===x && boxes[o][1]===y){
           max++;
           continue;
@@ -46,34 +49,48 @@ function App() {
   })
   
   const handleKeyDown=(e)=>{
-  
-    if(e.key==="q"){
-      const boxAbove=getGridElement(myX,myY-1,boxes)
-      if(!boxAbove){
-      if(myY>0) setMyY(myY-1)
-    }
-  }
-    
-    if(e.key==="a"){
-      const boxBelow=getGridElement(myX,myY+1,boxes)
-      if(!boxBelow){
-      if(myY<30) setMyY(myY+1)
-    }
-  }
-  
-   if(e.key==="p"){
-    const boxRight=getGridElement(myX+1,myY,boxes)
-    if(!boxRight){
-    if(myX<30) setMyX(myX+1)
-   }
-  }
-
-  if(e.key==="o"){
-     const boxLeft=getGridElement(myX-1,myY,boxes)
-     if(!boxLeft){
-    if(myX>0) setMyX(myX-1)
-   }
-  }
+          if(e.key==="q"){
+          const boxAbove=getGridElement(myX,myY-1,boxes)
+          if(!boxAbove){
+          if(myY>0) {
+            setMyY(myY-1)
+              if(!bombSet)setBombY(bombY-1)
+          }
+          }
+        }
+        if(e.key==="a"){
+          const boxBelow=getGridElement(myX,myY+1,boxes)
+          if(!boxBelow){
+          if(myY<30) {
+            setMyY(myY+1)
+              if(!bombSet)setBombY(bombY+1)
+          }
+          }
+        }
+        if(e.key==="p"){
+          const boxRight=getGridElement(myX+1,myY,boxes)
+          if(!boxRight){
+          if(myX<30) {
+            setMyX(myX+1)
+              if(!bombSet)setBombX(bombX+1)
+          }
+          }
+        }
+        if(e.key==="o"){
+          const boxLeft=getGridElement(myX-1,myY,boxes)
+          if(!boxLeft){
+          if(myX>0) {
+            setMyX(myX-1)
+            if(!bombSet)setBombX(bombX-1)
+          }
+          }
+        }
+        if(e.key==="z"){
+         // this doesnt work, or isnt rendering at least...
+          setBombX(myX)
+          setBombY(myY)
+          createElement("div",{className:'bomb',gridColumn:bombX,gridRow:bombY},"E")
+        }
 }
 
   return (
@@ -83,15 +100,13 @@ function App() {
     {boxes.map((box,index)=>{
       return <div className="tnt" style={{gridColumn:box[0],gridRow:box[1]}} key={index}>X</div>
     })}
+    <div id="bomb" style={{gridColumn:bombX,gridRow:bombY,visibility:false}}>.</div>
     <div ref= {inputRef} id="me" tabIndex={0} onKeyDown={handleKeyDown} style={{gridColumn:myX,gridRow:myY}}>S</div>
     <div id="guard"  style={{gridColumn:guardX,gridRow:guardY}}>G</div>
+    
        </div>
     </>
   )
 }
-
-
- 
- 
 
 export default App

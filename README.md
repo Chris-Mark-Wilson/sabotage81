0828f47ba0ffff20e09b8d9f47a328f3e0f78546
bigsegs <cw.plastering.services@gmail.com>
2023-07-29 17:58:59 +0100

    recursive explosions working

--- README.md
+++ README.md
@@ -20,3 +20,65 @@ so.. onwards..
 cool, now Ive got  bomb.. I rendered the bomb before "me" so that you see me and not the bomb when Im running around the screen, carrying it with me!
 maybe I'll code in a machine gun later just so I can say "now I too have a machine gun"...
 
+countdown set and working (press space to 'set' the bomb)
+
+now for the tricky bit, gonna have to pseudocode this one...
+We need a recursive function (do we?) yes probably - weve gotta set the bomb off.. and the idea is that a chain reaction occurs that sets off the neighbouring uxb's which in turn set off neighbouring uxb's and so on..
+now, last time I had a go at this (many years ago) when I got to this bit I found that the chain reaction pretty much wiped out the entire screen...
+These days Ive got the benefit of being able to acually PLAY the original version in an emulator.. and I noticed something...
+You cannot score more than 50 boxes at a time...
+So, the original dev must have had the same problem and 'capped' it off at 50 to solve it..
+Right then...
+
+//  boomTime - we gonna need an array... are we? not sure actually..
+look around bomb, add the locations of each uxb within a 1 grid radius to an ARRAY (see, we need an array)
+
+call function with an array containing bomb position - bangArray
+
+    set STOP=false - instate, top of file
+    
+     if !STOP{
+                
+                look around the position
+                add the positions of the to the testArray
+
+                loop through the testArray
+                        if the element at that position is a uxb, add to bangArray
+                        delete THIS position from the uxb array (important!) - removeBox()
+                        add 1 to score
+                        
+                if array.length>50 put a STOP flag up
+                remove 1st element from bang array
+                loop back around
+
+                ok that now works..
+    
+    sort of..
+    problem... bomb co-ordinates refuse to be reset to my position..
+    and I also need to generate an 'explosion' graphic at the site of the explosions.. but I dont know how to get the element by the grid co-ordinates..
+    looking into the following..
+    let e=document.getElementBYId("tnt")
+    let x=e.style.gridColumn
+    let y=e.style.gridRow
+    which gets the grid co-ordinates of an element
+
+    and
+
+    let e=document.getElementById("tnt")
+    e.style.gridColumn=x
+    e.style.gridColumn=y
+    which will set an element at a particular grid co-ordinate
+
+    gone teatime...
+    
+
+
+              
+
+
+
+
+    }
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -14,40 +14,43 @@ const getGridElement=(x,y,boxes)=>{
     }
      return false;
 }
-
+/////////////////////////////////////////////////////////////////////
 const App=()=>{
-  const [myX,setMyX]=useState(getRnd())
-  const [myY,setMyY]=useState(getRnd())
-  const [guardX,setGuardX]=useState(getRnd())
-  const [guardY,setGuardY]=useState(getRnd())
-  const [bombX,setBombX]=useState(myX)
-  const[bombY,setBombY]=useState(myY)
-  let     [count,setCount]=useState(9)
-let [bombSet,setBombSet]=useState(false);
+  let [myX,setMyX]=useState(getRnd())
+  let [myY,setMyY]=useState(getRnd())
+  let [guardX,setGuardX]=useState(getRnd())
+  let [guardY,setGuardY]=useState(getRnd())
+  let [bombX,setBombX]=useState(myX)
+  let [bombY,setBombY]=useState(myY)
+  let [count,setCount]=useState(10)
+  let [bombSet,setBombSet]=useState(false);
+  let [stop,setStop]=useState(false)
+  let [score,setScore]=useState(0)
   const inputRef=useRef(null) // used to set focus on player at start
   useEffect(() => {
     inputRef.current.focus();
 }, []);
 
   // state for array of uxb boxes
-  const [boxes,setBoxes]=useState(()=>{
+  let [boxes,setBoxes]=useState(()=>{
     let boxes=[]
     let max=300
     for(let i=0;i<max;i++){
       let x=Math.floor(Math.random()*31)
       let y=Math.floor(Math.random()*31)
     //check for duplication/overlay 
-    for (let o=0;o<boxes.length;o++){
+    boxes.push([x,y])
+    for (let o=0;o<boxes.length-1;o++){
         if(boxes[o][0]===x && boxes[o][1]===y){
-          max++;
-          continue;
+          boxes.pop()
+          i--;
          }
        }
-       boxes.push([x,y])
      }
      return boxes;
-  })
-  
+  },[])
+
+////////////////////////////////////////////////////
   const handleKeyDown=(e)=>{
           if(e.key==="q"){
           const boxAbove=getGridElement(myX,myY-1,boxes)
@@ -86,30 +89,103 @@ let [bombSet,setBombSet]=useState(false);
           }
         }
         if(e.key===" "){
+          console.log(boxes.length,"<-boxes length on fire")
      setBombSet(true);
           setCountdown()
         }
 }
 
+///////////////////////////////////////////////////////////////////
 const setCountdown=()=>{
- 
-  if(count>=0) setTimeout(tickdown,1000)
-}
-const tickdown=()=>{
-if(count===0)boomTime();
-count--;
-const bomb=document.getElementById("bomb")
-bomb.textContent=count;
-setCountdown()
-}
-const boomTime=()=>{
-  stopCount()
-  console.log("Bang!")
-}
-const stopCount=()=>{
-setBombSet(false);
+  if(count>=1) setTimeout(tickdown,1000)
 }
 
+const tickdown=()=>{
+    if(count===1)boomTime([[bombX,bombY]]);
+    count--;
+    const bomb=document.getElementById("bomb")
+    bomb.textContent=count;
+    setCountdown()
+}
+
+const stopCount=()=>{
+  setBombSet(false);
+}
+
+////////////////BAAAAAANNNNNGGGG//////////////////////////////
+
+const boomTime=(bangArray)=>{
+  stopCount()
+  console.log("Bang!")
+
+  if(bangArray.length===0){
+console.log("finished")
+console.log("score=",score)
+// setMyX(myX)
+// setMyY(myY)
+// setBombX(myX)
+// setBombY(myY)
+bombX=myX;
+bombY=myY;
+console.log("me->",myX,",",myY)
+console.log("bomb->",bombX,",",bombY)
+
+return
+  }
+
+ console.log(bangArray[0][0],"bangarray00")
+ console.log(boxes.length,"<-boxes length start")
+  if(!stop){
+    let testArray=[];
+    testArray.push([bangArray[0][0]-1,bangArray[0][1]-1]) // top left
+    testArray.push([bangArray[0][0],bangArray[0][1]-1]) // top mid
+    testArray.push([bangArray[0][0]+1,bangArray[0][1]-1]) // top right
+    testArray.push([bangArray[0][0]-1,bangArray[0][1]]) // left
+    testArray.push([bangArray[0][0]+1,bangArray[0][1]]) //right
+    testArray.push([bangArray[0][0]-1,bangArray[0][1]+1]) // bottom left
+    testArray.push([bangArray[0][0],bangArray[0][1]+1]) // bottom mid
+    testArray.push([bangArray[0][0]+1,bangArray[0][1]+1]) //bottom right
+
+
+    testArray.forEach(position=>{
+          boxes.forEach((box,index)=>{  //test for uxb and remove
+                if(box[0]===position[0] && box[1]===position[1]){
+                  bangArray.push(position);
+                  setBoxes(removeBox(index))
+                }
+          })
+    })
+
+    bangArray.shift();  //remove this position 
+    
+
+    if(bangArray.length>50)stop=true;
+    
+    boomTime(bangArray) // recursive call
+  }
+}
+
+//////////////////////////////////////
+
+const removeBox=(index)=>{
+  let stash=[]
+  console.log(index,"<-index")
+  for(let i=0;i<index;i++){
+    let shifted=boxes.shift()
+     // remove the front of the 'queue' and 'stash'
+    stash.push(shifted)
+  }
+  boxes.shift() //remove the box to blow up
+  setScore(score+=1)
+  // put the stash back
+  for(let i=stash.length-1;i>=0;i--){
+    boxes.unshift(stash[i])
+  }
+ 
+  return boxes
+}
+
+/////////////////////////////////////////////////////////////////////
   return (
     <>
       
@@ -117,7 +193,7 @@ setBombSet(false);
     {boxes.map((box,index)=>{
       return <div className="tnt" style={{gridColumn:box[0],gridRow:box[1]}} key={index}>X</div>
     })}
-    <div id="bomb" style={{gridColumn:bombX,gridRow:bombY,visibility:false}}>.</div>
+    <div id="bomb" style={{gridColumn:bombX,gridRow:bombY,visibility:false}}>10</div>
     <div ref= {inputRef} id="me" tabIndex={0} onKeyDown={handleKeyDown} style={{gridColumn:myX,gridRow:myY}}>S</div>
     <div id="guard"  style={{gridColumn:guardX,gridRow:guardY}}>G</div>
     

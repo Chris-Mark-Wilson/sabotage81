d52a0b1bf291c332e25f25aa8e668e1c5c5aa889
bigsegs <cw.plastering.services@gmail.com>
2023-10-16 09:13:46 +0100

    refactored det queue

--- src/utils/getDetonationQueue.js
+++ src/utils/getDetonationQueue.js
@@ -1,76 +1,45 @@
-const getDetonationQueue = (tnt,detonationQueue, limit, stop = false) => {
-  
-  //// FOR GODS SAKE DO NOT MESS WITH THIS FUNCTION YET!! ///////////
-
-  // called from app line 78 ish
-  //detonationQueue array initially set to bombPos
-  //limit is number of tnt to add to queue to avoid wiping out
-  //the entire screen in one shot, if limit not reached, no problem.
-  //detonationQueue should be an array of co-ordinate objects
-  //function returns the detonation queue to app
-  if (
-    stop ||
-    detonationQueue.length >= limit //base case reached
-  ) {
+const getDetonationQueue = (tntType, detonationQueue, limit, stop = false) => {
+  if (stop || detonationQueue.length >= limit) {
     console.log("Finished getting queue");
-
     return detonationQueue;
   }
-console.log(tnt)
-  const tntElements = tnt==="X"?Array.from(document.getElementsByClassName("x")):tnt==="☠"?Array.from(document.getElementsByClassName("skull")):Array.from(document.getElementsByClassName("tnt"))
-  //tnt elements = array of tnt divs existing on screen
+
+  const tntSelector = tntType === "X" ? ".x" : tntType === "☠" ? ".skull" : ".tnt";
+  const tntElements = Array.from(document.querySelectorAll(tntSelector));
 
   const tntPositions = tntElements.map((element) => {
-    const obj = {};
-  
-    obj.x = +element.style.gridArea.split("/")[1];
-    obj.y = +element.style.gridArea.split("/")[0];
-    return obj;
+    const [y, x] = element.style.gridArea.split("/").map(Number);
+    return { x, y };
   });
-  //tnt positions = array of position objects {x,y}
-  // corresponding to tnt divs on screen
 
-  let newBoxes = []; // any found tnt boxes in range go in here
+  let newBoxes = [];
 
   detonationQueue.forEach((centre) => {
-    // iterate through the queue
+    const testArray = [
+      { x: -1, y: -1 }, { x: 0, y: -1 }, { x: 1, y: -1 },
+      { x: -1, y: 0 },                  { x: 1, y: 0 },
+      { x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 },
+    ].map(offset => ({ x: centre.x + offset.x, y: centre.y + offset.y }));
 
-    let testArray = []; //8 grid co-ord objects {x,y} around current position
-
-    testArray.push({ x: centre.x - 1, y: centre.y - 1 }); // top left
-    testArray.push({ x: centre.x, y: centre.y - 1 }); // top mid
-    testArray.push({ x: centre.x + 1, y: centre.y - 1 }); // top right
-    testArray.push({ x: centre.x - 1, y: centre.y }); // left
-    testArray.push({ x: centre.x + 1, y: centre.y }); //right
-    testArray.push({ x: centre.x - 1, y: centre.y + 1 }); // bottom left
-    testArray.push({ x: centre.x, y: centre.y + 1 }); // bottom mid
-    testArray.push({ x: centre.x + 1, y: centre.y + 1 }); //bottom right
-
-    //iterate through testArray, find any live tnt boxes
-    const liveBoxes = testArray.filter((testPosition) => {
-      return (
-        tntPositions.some(
-          (tntPosition) =>
-            tntPosition.x === testPosition.x && tntPosition.y === testPosition.y
-        ) &&
-        !detonationQueue.some(
-          (position) =>
-            position.x === testPosition.x && position.y === testPosition.y
-        )
-      );
-      // if tnt at testPosition, and isnt already in queue... add testPosition to liveBoxes
-    });
-    //liveBoxes = array of all the tnt position surrounding current position
+    const liveBoxes = testArray.filter((testPosition) =>
+      tntPositions.some(
+        (tntPosition) =>
+          tntPosition.x === testPosition.x && tntPosition.y === testPosition.y
+      ) &&
+      !detonationQueue.some(
+        (position) =>
+          position.x === testPosition.x && position.y === testPosition.y
+      )
+    );
 
     if (liveBoxes.length === 0) {
-      stop = true; // stop flag for recursive call
+      stop = true;
     } else {
-      newBoxes = [...newBoxes, ...liveBoxes];
+      newBoxes.push(...liveBoxes);
     }
-  }); // end detonationQueue.forEach()
+  });
 
-  //add newBoxes to detonationQueue and return
-  return getDetonationQueue(tnt,[...detonationQueue, ...newBoxes], limit, stop); //recursive call
+  return getDetonationQueue(tntType, [...detonationQueue, ...newBoxes], limit, stop);
 };
 
 export default getDetonationQueue;
